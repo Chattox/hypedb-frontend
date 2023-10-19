e4a7b67bce75f365d419476e2f273b6b64039628
Conrad Lineker-Jones <33847899+Chattox@users.noreply.github.com>
2023-10-19 22:44:13 +0100

    feat(theme): add custom colour scheme
    * feat(theme): custom colour scheme
    
    * feat(theme): update colour scheme
    
    * feat(theme): primary colour

---
+++ src/theme.ts
@@ -0,0 +1,29 @@
+import { DefaultMantineColor, MantineColorsTuple, createTheme } from '@mantine/core';
+import { generateColors } from '@mantine/colors-generator';
+
+type ExtendedCustomColors =
+  | 'beige'
+  | 'sunglow'
+  | 'airblue'
+  | 'forestgreen'
+  | 'gunmetal'
+  | DefaultMantineColor;
+
+declare module '@mantine/core' {
+  export interface MantineThemeColorsOverride {
+    colors: Record<ExtendedCustomColors, MantineColorsTuple>;
+  }
+}
+
+export const customeTheme = createTheme({
+  primaryColor: 'forestgreen',
+  white: 'var(--mantine-color-beige-0)',
+  black: 'var(--mantine-color-gunmetal-9)',
+  colors: {
+    beige: generateColors('#EEF4D4'),
+    sunglow: generateColors('#FFCE5C'),
+    airblue: generateColors('#7CA5B8'),
+    forestgreen: generateColors('#2E933C'),
+    gunmetal: generateColors('#1C2826'),
+  },
+});
