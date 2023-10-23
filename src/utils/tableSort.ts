7fd1f03d6467a59514bd7727c2d296a10057a663
Chattox <conradlineker@gmail.com>
2023-10-23 15:11:17 +0100

    feat(table-sorting): sort text columns

---
+++ src/utils/tableSort.ts
@@ -0,0 +1,34 @@
+export const tableSort = (tableState: TableStateProps, column: Column): TableStateProps => {
+  const newSortOrder =
+    tableState.sortColumn === column.name
+      ? tableState.sortOrder === 'asc'
+        ? 'desc'
+        : 'asc'
+      : 'asc';
+  console.log(`Old sort order: ${tableState.sortOrder}, new sort order: ${newSortOrder}`);
+  console.log(`Old column name: ${tableState.sortColumn}, new column name: ${column.name}`);
+
+  switch (column.type) {
+    case 'text':
+      return {
+        ...tableState,
+        sortColumn: column.name,
+        sortOrder: newSortOrder,
+        gamesData:
+          newSortOrder === 'asc'
+            ? [...tableState.gamesData].sort((a, b) =>
+                (a[column.accessor] as string).toLowerCase() <
+                (b[column.accessor] as string).toLowerCase()
+                  ? -1
+                  : 1
+              )
+            : [...tableState.gamesData].sort((a, b) =>
+                (a[column.accessor] as string).toLowerCase() >
+                (b[column.accessor] as string).toLowerCase()
+                  ? -1
+                  : 1
+              ),
+      };
+  }
+  return tableState;
+};
