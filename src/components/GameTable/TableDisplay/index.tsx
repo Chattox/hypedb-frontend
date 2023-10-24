c165499b27cead12de482c2be746e954f1b2708e
Chattox <conradlineker@gmail.com>
2023-10-24 15:29:54 +0100

    refactor(gamedata): hoist game data query to appcontainer

--- src/components/GameTable/TableDisplay/index.tsx
+++ src/components/GameTable/TableDisplay/index.tsx
@@ -1,21 +1,24 @@
-import { Table } from '@mantine/core';
-import { DeleteGame } from '../../DeleteGame';
-import { EditGame } from '../../EditGame';
-import { useEffect, useState } from 'react';
-import { tableSort } from '../../../utils/tableSort';
+import { Loader, Table } from "@mantine/core";
+import { DeleteGame } from "../../DeleteGame";
+import { EditGame } from "../../EditGame";
+import { useEffect, useState } from "react";
+import { tableSort } from "../../../utils/tableSort";
 
-export const TableDisplay = (props: { gameData: GameTableEntry[] }) => {
+export const TableDisplay = (props: {
+  isLoading: boolean;
+  gameData: GameTableEntry[];
+}) => {
   const [state, setState] = useState<TableStateProps>({
     gamesData: props.gameData,
-    sortOrder: 'desc',
-    sortColumn: 'Title',
+    sortOrder: "desc",
+    sortColumn: "Title",
     loaded: false,
   });
 
   useEffect(() => {
     if (state.loaded && state.gamesData.length > 0) {
       const newState = tableSort(state, columns[0]);
-      setState({ ...state, sortOrder: 'asc', gamesData: newState.gamesData });
+      setState({ ...state, sortOrder: "asc", gamesData: newState.gamesData });
     } else {
       setState({
         ...state,
@@ -27,14 +30,39 @@ export const TableDisplay = (props: { gameData: GameTableEntry[] }) => {
   }, [props.gameData, state.loaded]);
 
   const columns: Column[] = [
-    { name: 'Title', type: 'text', isSortable: true, accessor: 'name' },
-    { name: 'Genre', type: 'text', isSortable: true, accessor: 'genre' },
-    { name: 'Link', type: 'text', isSortable: false, accessor: 'linkUrl' },
-    { name: 'Description', type: 'text', isSortable: false, accessor: 'description' },
-    { name: 'HypeScore', type: 'numeric', isSortable: true, accessor: 'hypeScore' },
-    { name: 'Release Date', type: 'releaseDate', isSortable: true, accessor: 'releaseDate' },
-    { name: 'Date Added', type: 'date', isSortable: true, accessor: 'createdAt' },
-    { name: 'Last Updated', type: 'date', isSortable: true, accessor: 'updatedAt' },
+    { name: "Title", type: "text", isSortable: true, accessor: "name" },
+    { name: "Genre", type: "text", isSortable: true, accessor: "genre" },
+    { name: "Link", type: "text", isSortable: false, accessor: "linkUrl" },
+    {
+      name: "Description",
+      type: "text",
+      isSortable: false,
+      accessor: "description",
+    },
+    {
+      name: "HypeScore",
+      type: "numeric",
+      isSortable: true,
+      accessor: "hypeScore",
+    },
+    {
+      name: "Release Date",
+      type: "releaseDate",
+      isSortable: true,
+      accessor: "releaseDate",
+    },
+    {
+      name: "Date Added",
+      type: "date",
+      isSortable: true,
+      accessor: "createdAt",
+    },
+    {
+      name: "Last Updated",
+      type: "date",
+      isSortable: true,
+      accessor: "updatedAt",
+    },
   ];
 
   const rows = state.gamesData.map((game) => (
@@ -45,8 +73,8 @@ export const TableDisplay = (props: { gameData: GameTableEntry[] }) => {
       <Table.Td>{game.description}</Table.Td>
       <Table.Td>{game.hypeScore}</Table.Td>
       <Table.Td>{game.releaseDate.displayString}</Table.Td>
-      <Table.Td>{game.createdAt.format('{numeric-uk} {time-24}')}</Table.Td>
-      <Table.Td>{game.updatedAt.format('{numeric-uk} {time-24}')}</Table.Td>
+      <Table.Td>{game.createdAt.format("{numeric-uk} {time-24}")}</Table.Td>
+      <Table.Td>{game.updatedAt.format("{numeric-uk} {time-24}")}</Table.Td>
       <Table.Td>
         <EditGame gameValues={game} />
       </Table.Td>
@@ -67,14 +95,16 @@ export const TableDisplay = (props: { gameData: GameTableEntry[] }) => {
           {columns.map((column) => (
             <Table.Th
               key={column.name}
-              onClick={column.isSortable ? () => handleOnClick(column) : undefined}
+              onClick={
+                column.isSortable ? () => handleOnClick(column) : undefined
+              }
             >
               {column.name}
             </Table.Th>
           ))}
         </Table.Tr>
       </Table.Thead>
-      <Table.Tbody>{rows}</Table.Tbody>
+      {props.isLoading ? <Loader /> : <Table.Tbody>{rows}</Table.Tbody>}
     </Table>
   );
 };
--- src/components/GameTable/index.tsx
+++ src/components/GameTable/index.tsx
@@ -1,24 +1,13 @@
-import { useEffect, useState } from 'react';
-import { useQuery } from '@apollo/client';
-import { GET_GAMES } from '../../utils/operations';
-import { formatData } from '../../utils/formatData';
-import { TableDisplay } from './TableDisplay';
-import { Box } from '@mantine/core';
+import { TableDisplay } from "./TableDisplay";
+import { Box } from "@mantine/core";
 
-export const GameTable = () => {
-  const { loading, error, data } = useQuery(GET_GAMES);
-  const [formattedData, setFormattedData] = useState<GameTableEntry[]>([]);
-
-  useEffect(() => {
-    if (!loading && !error) {
-      setFormattedData(formatData(data.games));
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [loading]);
-
-  if (error) {
-    return <p>Error: {`${error.message}`}</p>;
-  }
-
-  return <Box>{loading ? <p>Loading</p> : <TableDisplay gameData={formattedData} />}</Box>;
+export const GameTable = (props: {
+  isLoading: boolean;
+  gameData: GameTableEntry[];
+}) => {
+  return (
+    <Box>
+      <TableDisplay isLoading={props.isLoading} gameData={props.gameData} />
+    </Box>
+  );
 };
