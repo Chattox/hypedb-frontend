2051b73036f4d94ac5c00a06a8df370bb5484d76
Chattox <conradlineker@gmail.com>
2023-10-25 15:28:18 +0100

    feat(theme): new colour scheme

--- src/theme.ts
+++ src/theme.ts
@@ -1,29 +1,99 @@
-import { DefaultMantineColor, MantineColorsTuple, createTheme } from '@mantine/core';
-import { generateColors } from '@mantine/colors-generator';
+import {
+  DefaultMantineColor,
+  MantineColorsTuple,
+  createTheme,
+} from "@mantine/core";
 
 type ExtendedCustomColors =
-  | 'beige'
-  | 'sunglow'
-  | 'airblue'
-  | 'forestgreen'
-  | 'gunmetal'
+  | "hypePurplePrimary"
+  | "hypePink"
+  | "hypePurpleSecondary"
+  | "hypeBlue"
+  | "hypeGreen"
+  | "hypeYellow"
   | DefaultMantineColor;
 
-declare module '@mantine/core' {
+declare module "@mantine/core" {
   export interface MantineThemeColorsOverride {
     colors: Record<ExtendedCustomColors, MantineColorsTuple>;
   }
 }
 
-export const customeTheme = createTheme({
-  primaryColor: 'forestgreen',
-  white: 'var(--mantine-color-beige-0)',
-  black: 'var(--mantine-color-gunmetal-9)',
+export const customTheme = createTheme({
+  primaryColor: "hypePurplePrimary",
+  white: "var(--mantine-color-hypePurplePrimary-1)",
   colors: {
-    beige: generateColors('#EEF4D4'),
-    sunglow: generateColors('#FFCE5C'),
-    airblue: generateColors('#7CA5B8'),
-    forestgreen: generateColors('#2E933C'),
-    gunmetal: generateColors('#1C2826'),
+    hypePurplePrimary: [
+      "#f3ecfd",
+      "#dfccfb",
+      "#dbc6fa",
+      "#cfb4f9",
+      "#c3a1f7",
+      "#b78ef6",
+      "#ab7bf4",
+      "#a068f3",
+      "#9455f1",
+      "#8842f0",
+    ],
+    hypePink: [
+      "#feebf9",
+      "#fed8f3",
+      "#fdc4ed",
+      "#fca6e4",
+      "#fb9de1",
+      "#fb89da",
+      "#fa75d4",
+      "#f962ce",
+      "#f94ec8",
+      "#f83ac2",
+    ],
+    hypePurpleSecondary: [
+      "#f1eefc",
+      "#e3ddf8",
+      "#d5ccf5",
+      "#c8bbf2",
+      "#baaaee",
+      "#a08be8",
+      "#9e88e7",
+      "#9077e4",
+      "#8266e1",
+      "#7555dd",
+    ],
+    hypeBlue: [
+      "#efecfd",
+      "#e0dafc",
+      "#c8bdf9",
+      "#c1b4f8",
+      "#b1a2f6",
+      "#a28ff5",
+      "#927cf3",
+      "#836af1",
+      "#7357ef",
+      "#6344ee",
+    ],
+    hypeGreen: [
+      "#adf32b",
+      "#a5f218",
+      "#9be70d",
+      "#8ed40c",
+      "#81c10b",
+      "#6ba009",
+      "#679a09",
+      "#5a8708",
+      "#4e7407",
+      "#416006",
+    ],
+    hypeYellow: [
+      "#fffceb",
+      "#fff8c9",
+      "#fff7c2",
+      "#fff4ad",
+      "#fff199",
+      "#ffef85",
+      "#ffec70",
+      "#ffe95c",
+      "#ffe747",
+      "#ffe433",
+    ],
   },
 });
