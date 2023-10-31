74db5dd0907063c71febd5cfec7e75a98a03ca1f
Chattox <conradlineker@gmail.com>
2023-10-31 11:51:38 +0000

    feat(game-form): specific release date picker styling

--- src/components/GameForm/ReleaseDatePicker/index.tsx
+++ src/components/GameForm/ReleaseDatePicker/index.tsx
@@ -3,9 +3,8 @@ import { useEffect, useState } from "react";
 import ReactDatePicker from "react-datepicker";
 import spacetime from "spacetime";
 import { capitalise } from "../../../utils/capitalise";
-import "react-datepicker/dist/react-datepicker.module.css";
 import classes from "./ReleaseDatePicker.module.css";
-import "./teststyling.scss";
+import "./ReleaseDatePicker.scss";
 
 type ReleaseDatePickerProps = {
   value: ReleaseDateInput;
@@ -30,11 +29,15 @@ export const ReleaseDatePicker = (props: ReleaseDatePickerProps) => {
     releaseDate.dateType === "custom" ? releaseDate.dateString : ""
   );
 
-  const handleOnChange = async (changeType: "dateType" | "dateString" | "custom", value: any) => {
+  const handleOnChange = async (
+    changeType: "dateType" | "dateString" | "custom",
+    value: any
+  ) => {
     if (changeType === "dateType") {
       setReleaseDate({
         dateType: value.toLowerCase(),
-        dateString: value === "Custom" ? customReleaseDate : spacetime().format("iso"),
+        dateString:
+          value === "Custom" ? customReleaseDate : spacetime().format("iso"),
       });
     } else if (changeType === "dateString") {
       const relDate = spacetime(value);
@@ -52,7 +55,9 @@ export const ReleaseDatePicker = (props: ReleaseDatePickerProps) => {
   useEffect(() => {
     setReleaseDate({
       ...releaseDate,
-      dateString: customReleaseDate ? customReleaseDate : releaseDate.dateString,
+      dateString: customReleaseDate
+        ? customReleaseDate
+        : releaseDate.dateString,
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [customReleaseDate]);
@@ -93,7 +98,9 @@ export const ReleaseDatePicker = (props: ReleaseDatePickerProps) => {
             label="Custom release date"
             placeholder="Coming Soon™"
             value={customReleaseDate}
-            onChange={(event) => handleOnChange("custom", event.currentTarget.value)}
+            onChange={(event) =>
+              handleOnChange("custom", event.currentTarget.value)
+            }
           />
         );
     }
@@ -105,7 +112,9 @@ export const ReleaseDatePicker = (props: ReleaseDatePickerProps) => {
         label="Date type"
         data={["Specific", "Quarter", "Year", "Custom"]}
         value={capitalise(releaseDate.dateType)}
-        onChange={(event) => handleOnChange("dateType", event.currentTarget.value)}
+        onChange={(event) =>
+          handleOnChange("dateType", event.currentTarget.value)
+        }
         className={classes.dateTypeSelect}
       />
       {getPicker()}
--- src/theme.ts
+++ src/theme.ts
@@ -22,7 +22,7 @@ declare module "@mantine/core" {
 
 export const customTheme = createTheme({
   primaryColor: "hypePurplePrimary",
-  black: "var(--mantine-color-hypePurpleSecondary-9)",
+  black: "var(--mantine-color-hypePurplePrimary-9)",
   colors: {
     hypePurplePrimary: [
       "#f3ecfd",
